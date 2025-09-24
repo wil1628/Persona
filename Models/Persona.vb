e9406fc1e2ea8068f27402bface4f033b824de39
wil1628 <fofojimenez16@gmail.com>
2025-09-23 20:01:38 -0600

    Agregar archivos de proyecto.

---
+++ Models/Persona.vb
@@ -0,0 +1,44 @@
+﻿Public Class Persona
+
+    Private _nombre As String
+    Private _apellido As String
+    Private _edad As Integer
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As String
+        Get
+            Return _apellido
+        End Get
+        Set(value As String)
+            _apellido = value
+        End Set
+    End Property
+
+    Public Property Edad As Integer
+        Get
+            Return _edad
+        End Get
+        Set(value As Integer)
+            _edad = value
+        End Set
+    End Property
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(nombre As String, apellido As String, edad As Integer)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Edad = edad
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Persona")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("Persona")>
+<Assembly: AssemblyCopyright("Copyright © HP 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("250695d1-0d90-4351-8b49-2b36342ecf0d")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
