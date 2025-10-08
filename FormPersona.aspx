74abe2ab1a582145b87a12e312c5e7b492c05827
wil1628 <fofojimenez16@gmail.com>
2025-10-07 18:14:46 -0600

    Conexion DB

---
+++ FormPersona.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="FormPersona.aspx.vb" Inherits="Persona.FormPersona" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="gvPersonas" runat="server" AutoGenerateColumns="False" DataKeyNames="ID" DataSourceID="SqlDataSource">
+        <Columns>
+            <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="True" SortExpression="ID" />
+            <asp:BoundField DataField="nombre" HeaderText="nombre" SortExpression="nombre" />
+            <asp:BoundField DataField="apellido" HeaderText="apellido" SortExpression="apellido" />
+            <asp:BoundField DataField="edad" HeaderText="edad" SortExpression="edad" />
+        </Columns>
+</asp:GridView>
+<asp:SqlDataSource ID="SqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:II46ConnectionString %>" ProviderName="<%$ ConnectionStrings:II46ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Persona]"></asp:SqlDataSource>
+</asp:Content>
+
---
+++ FormPersona.aspx.designer.vb
@@ -0,0 +1,33 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class FormPersona
+
+    '''<summary>
+    '''Control gvPersonas.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gvPersonas As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control SqlDataSource.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource As Global.System.Web.UI.WebControls.SqlDataSource
+End Class
